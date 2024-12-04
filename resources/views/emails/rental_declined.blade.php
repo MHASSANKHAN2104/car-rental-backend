638345e734df1a0724295a769102275a74170623
HASSAN\User <pc16811.hassankhan@gmail.com>
2024-12-04 19:35:55 +0500

    no message

--- resources/views/emails/rental_declined.blade.php
+++ resources/views/emails/rental_declined.blade.php
@@ -12,6 +12,26 @@
 
     <h3>Rental Request Declined</h3>
     <p>Dear {{ $customer->first_name }} {{ $customer->last_name }},</p>
-    <p>{{ $declineMessage }}</p>  <!-- This will display the rejection message -->
+    <p>{{ $declineMessage }}</p>
+
+    <h3>Rental Details:</h3>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
+            <tr>
+                <th>Customer Name</th>
+                <th>Phone Number</th>
+                <th>Address</th>
+                <th>Vehicle Information</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $customer->first_name }} {{ $customer->last_name }}</td>
+                <td>{{ $customer->phone_number }}</td>
+                <td>{{ $customer->address }}</td>
+                <td>{{ $vehicle->brand }} {{ $vehicle->model }} ({{ $vehicle->reg_number }})</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
